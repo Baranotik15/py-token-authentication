53fb76a41b3aaf32f681e3830538dceadabd17bc
RuslanTopal <baranotik15@gmail.com>
2025-04-29 21:57:19 +0000

    added creating token

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from user.views import UserViewSet
+from user.views import CreateUserView, CreateTokenView
+
 
 
 urlpatterns = [
-    path("register/", UserViewSet.as_view(), name="register"),
-    path("login/", UserViewSet.as_view(), name="login"),
-    path("me/", UserViewSet.as_view(), name="me"),
+    path("register/", CreateUserView.as_view(), name="register"),
+    path("login/", CreateTokenView.as_view(), name="token"),
+    path("me/", CreateUserView.as_view(), name="me"),
 ]
+
+app_name = "user"
--- user/views.py
+++ user/views.py
@@ -1,8 +1,13 @@
-from rest_framework import viewsets
+from rest_framework import generics
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 from user.serializers import UserSerializer
 
 
-class UserViewSet(viewsets.ModelViewSet):
+class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
 
+
+class CreateTokenView(ObtainAuthToken):
+   renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
