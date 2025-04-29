196ec2948e25936ed054291e81d582dd9aa91ed0
RuslanTopal <baranotik15@gmail.com>
2025-04-29 21:45:40 +0000

    crate funtionality for user

--- user/urls.py
+++ user/urls.py
@@ -1,1 +1,10 @@
-# write your code here
+from django.urls import path
+
+from user.views import UserViewSet
+
+
+urlpatterns = [
+    path("register/", UserViewSet.as_view(), name="register"),
+    path("login/", UserViewSet.as_view(), name="login"),
+    path("me/", UserViewSet.as_view(), name="me"),
+]
--- user/views.py
+++ user/views.py
@@ -1,1 +1,8 @@
-# write your code here
+from rest_framework import viewsets
+
+from user.serializers import UserSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    serializer_class = UserSerializer
+
