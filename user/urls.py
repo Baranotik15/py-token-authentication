42ac9e70cae28fc512652ac6085ddb41b2cf6e35
RuslanTopal <baranotik15@gmail.com>
2025-04-29 22:14:09 +0000

    added functionality for me endpoint

--- user/urls.py
+++ user/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from user.views import CreateUserView, CreateTokenView
 
 
-
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="register"),
     path("login/", CreateTokenView.as_view(), name="token"),
